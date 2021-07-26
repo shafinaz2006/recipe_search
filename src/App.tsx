157279ce6faf274099f8e8466fca1a4d0fc77288
Shafinaz Shafique <shafinaz2006@gmail.com>
2021-07-25 21:12:03 -0700

    update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  interface Recipe {
+    recipe: {
+      ingredientLines: string[];
+      label: string;
+      calories: string;
+      image: string;
+      url: string;
+    }
+    _links:{};
+  }
+  const [recipeFound, setRecipeFound] = useState<Recipe[]>([]);
+  const [recipeSearch, setRecipeSearch] = useState('');
+  const [display, setDisplay] = useState(false);
+ 
+  const getSearchString = (event: ChangeEvent<HTMLInputElement>): void  =>{
+    setRecipeSearch(event.target.value);
+  }
+  const searchRecipe = (event: FormEvent<HTMLFormElement>): void =>{
+    event.preventDefault();
+    // console.log(recipeSearch);
+    axios
+      .get(`https://api.edamam.com/api/recipes/v2?type=public&q=${recipeSearch}&app_id=202b54b2&app_key=aacb28831d67e34e10029db642233e04%09&imageSize=REGULAR`)
+      .then(response => setRecipeFound(response.data.hits))
+      .catch(error => console.log('Error in response data', error));
+  }
+
+  useEffect(() =>{
+    setDisplay(true);
+  }, [recipeFound]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1> Recipe Search App</h1>
+      <form onSubmit={event => searchRecipe(event)}>
+        <label htmlFor='search'>Enter your query: </label>
+        <input type='text'  id='search' name='recipeSearch' onChange={getSearchString} value={recipeSearch} placeholder='query'/>
+        <button >Search</button>
+      </form>
+      <div className='allRecipes'>
+      {display? recipeFound.map((recipe:any, i) => 
+        <section key={i} className='recipe'>
+          <img src={recipe.recipe.image} alt='recipeImage' className='recipeImage'/>
+          <p className='recipeLabel'>{recipe.recipe.label}</p>
+          <p className='recipeCalorie'>Calories: {recipe.recipe.calories}</p>
+          <div className='recipeIngredient'>
+            <p className='recipeIngLabel'>Ingredients:</p>
+            {recipe.recipe.ingredientLines? 
+            recipe.recipe.ingredientLines.map((ing:any, j:number) => <p key={j}> {ing}</p>): ''}
+          </div>
+          <a href={recipe.recipe.url} target='__blank' className='recipeDetails'> Details </a>
+        </section>
+      ) : '<h1> No recipe found in this category </h1>'}
+      </div>
+    </main>
   );
 }
 
